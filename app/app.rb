c5c1d43e688c7591028bc6285d883346765b80db
Daniel Steele <danielsteele@hotmail.co.uk>
2015-07-27 17:27:52 +0100

    Added defense checking it's an actual app

--- app/app.rb
+++ app/app.rb
@@ -10,7 +10,16 @@ class App
 
     def send_to_target_app(request)
       @target_app = request.path.split("/")[1].titleize
-      @target_app.constantize.new(request: request).to_controller
+      if current_apps.include? @target_app
+        @target_app.constantize.new(request: request).to_controller
+      else
+        raise "First part of path must be a valid controller"
+      end
     end
+
+    def current_apps
+      ["Techlog"]
+    end
+
   end
 end
