7d78458ab659e8f4aae68ae772bc7ec51b105f0e
Daniel Steele <danielsteele@hotmail.co.uk>
2015-07-27 14:05:17 +0100

    Implemented db_cleaner

--- spec/requests_spec.rb
+++ spec/requests_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
+require 'database_cleaner'
 
 describe "it should handle basic API requests" do
+  DatabaseCleaner.strategy = :truncation
 
   before do
     SitesFixture::sites.each do |site|
@@ -12,5 +14,9 @@ describe "it should handle basic API requests" do
     get "/techlog/sites"
     expect(last_response.ok?).to be true
     expect(last_response.body).to be SitesFixture::sites
+  end
+
+  after do
+    DatabaseCleaner.clean
   end
 end
