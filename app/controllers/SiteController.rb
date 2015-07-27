bc6ea8db2158847c3603355ad61cf82039472f5b
Daniel Steele <danielsteele@hotmail.co.uk>
2015-07-27 17:02:21 +0100

    Implemented show action

--- app/controllers/SiteController.rb
+++ app/controllers/SiteController.rb
@@ -3,6 +3,12 @@ class SiteController < Techlog
     super
   end
 
+  def show(ids)
+    @response.status = 200
+    @response.write Site.find(ids[:site]).to_json
+    @response
+  end
+
   def index
     @response.status = 200
     @response.write Site.all.to_json
