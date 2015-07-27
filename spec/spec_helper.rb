4ecc88a8239d5df6137c39da4c94263e174e4376
Daniel Steele <danielsteele@hotmail.co.uk>
2015-07-27 11:12:28 +0100

    Added configs for rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,15 @@ require 'pry'
 require './config/requires'
 require 'rack/test'
 include Rack::Test::Methods
+
+RSpec.configure do |config|
+  config.mock_with :rspec
+  config.expect_with :rspec
+  config.raise_errors_for_deprecations!
+end
+
+require 'capybara/rspec'
+Capybara.configure do |config|
+  config.app = Earlymarket::App.new
+  config.server_port = 9293
+end
