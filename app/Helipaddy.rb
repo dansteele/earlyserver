8b7596f4c011c232fb0895cb5d40b6309ee0f25b
Daniel Steele <danielsteele@hotmail.co.uk>
2015-07-28 10:18:23 +0100

    Inheriting from app

---
+++ app/Helipaddy.rb
@@ -0,0 +1,2 @@
+class Helipaddy < App
+end
--- app/app.rb
+++ app/app.rb
@@ -2,6 +2,8 @@ require 'rack'
 require 'rack/server'
 
 class App
+  attr_accessor :request
+
   class << self
     def call(env)
       request = Rack::Request.new env
@@ -13,7 +15,7 @@ class App
       if current_apps.include? @target_app
         @target_app.constantize.new(request: request).to_controller
       else
-        raise "First part of path must be a valid controller"
+        raise "First part of path must be a valid app. #{@target_app} is not an app."
       end
     end
 
@@ -21,5 +23,60 @@ class App
       ["Techlog"]
     end
 
+  end
+
+  def initialize(obj)
+    @request = obj[:request]
+    @request[:ids] = retrieve_ids
+    @response = Rack::Response.new
+  end
+
+  def to_controller
+    to_controller_name(get_controller_name).new(request: request).invoke_action
+  end
+
+  def get_controller_name
+    useful_path.find_last_word
+  end
+
+  def useful_path
+    @request.path.split("/")[2..-1]
+  end
+
+  def retrieve_ids
+    useful_path.select.with_index do |item, index|
+      if item.is_i?
+        return {useful_path[index-1].to_sym => item} 
+      end
+    end
+  end
+
+  def to_controller_name word
+    (word.titleize + "Controller").constantize
+  end
+
+  def invoke_action
+    # Assuming GET for now.
+    # Header checks will go here.
+    if useful_path.last.is_i?
+      self.send "show", @request[:ids]
+    else
+      self.send "index"
+    end
+  end
+
+
+  def show(*args)
+    @response.status = 200
+    # Set headers
+    # etc
+    @response
+  end
+
+  def index(*args)
+    @response.status = 200
+    # Set headers
+    # etc
+    @response
   end
 end
